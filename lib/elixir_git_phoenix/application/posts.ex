d58e4c0a774eb3c2796c5491f1714e0d5a4a97f0
samarthpkulkarni11 <samarthpkulkarni11@gmail.com>
2024-11-10 22:25:33 +0530

    Pushing some files

--- lib/elixir_git_phoenix/application/posts.ex
+++ lib/elixir_git_phoenix/application/posts.ex
@@ -3,17 +3,21 @@ defmodule ElixirGitPhoenix.Application.Posts do
 
   import Ecto.Changeset
 
+  @primary_key {:id, UUIDv7, autogenerate: true}
+  @foreign_key_type UUIDv7
   schema "posts" do
     field :caption, :string
     field :content_url, :string
     field :location, :string
 
-    belongs_to :user, ElixirGitPhoenix.Users.User
+    belongs_to :user, ElixirGitPhoenix.Users.User, foreign_key: :user_id, type: UUIDv7
+    timestamps(type: :utc_datetime)
   end
 
   def changeset(post, attrs) do
     post
     |> cast(attrs, [:caption, :location])
     |> validate_required([:caption, :location])
+    |> foreign_key_constraint(:user_id)
   end
 end
--- lib/elixir_git_phoenix_web/controllers/post_json.ex
+++ lib/elixir_git_phoenix_web/controllers/post_json.ex
@@ -1,2 +1,11 @@
 defmodule ElixirGitPhoenixWeb.PostJSON do
+  use ElixirGitPhoenix.Application.Posts
+
+  def index(%{posts: posts}) do
+    %{data: for(posts <- posts, do: post_data(user))}
+  end
+
+  def data(nil), do: nil
+
+  def show(%{post: post})
 end
