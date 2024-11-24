edf9280c87c0826f6fb1134939600eaa912d5c43
samarthkulkarni-cb <samarthkulkarni@channelblend.in>
2024-11-25 00:46:22 +0530

    feat(users): add posts schema and migrations for the schema. - Added more data points to the posts schema to have a valid post with things associated to the posts. - Migrated the data point changes to the database

--- lib/elixir_git_phoenix/application/posts.ex
+++ lib/elixir_git_phoenix/application/posts.ex
@@ -9,11 +9,16 @@ defmodule ElixirGitPhoenix.Application.Posts do
     field :caption, :string
     field :content_url, :string
     field :location, :string
+    field :hashtags, :string
+    field :likes_count, :integer
+    field :comments_count, :integer
+    field :is_public, :boolean
 
     belongs_to :user, ElixirGitPhoenix.Users.User, foreign_key: :user_id, type: UUIDv7
     timestamps(type: :utc_datetime)
   end
 
+  # the changeset for th
   def changeset(post, attrs) do
     post
     |> cast(attrs, [:caption, :location])
